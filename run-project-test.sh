b8e5b989e3af0b27c9d5e3848c932afd90e53511
Shijian Su <sushijian.iss.nus@gmail.com>
2020-05-05 22:03:06 +0800

    Batch commit and push by gpush.sh at 2020-05-05T22:01:17

--- run-project-test.sh
+++ run-project-test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# utilities to print with prefix
 function log {
   local message=$1
   echo "[PROJECT TEST] $message"
 }
 
+# execute maven clean test if there is pom.xml
 function execute_maven {
   local d="$1"
-  if [[ -f pom.xml ]]; then # maven project
-    printf "[PROJECT TEST] %s: " "$d" # no new line
+  # use "pom.xml" to tell if maven project
+  if [[ -f pom.xml ]]; then
+    printf "[PROJECT TEST] %s: " "$d" # printf not to printing a line seperator
+    # choose the maven exector, use maven wrapper or maven installed in local
     if [[ -f mvnw ]]; then response=$(./mvnw clean test); else response=$(mvn clean test); fi
+    # use "BUILD SUCCESS" as successful indicator
     if [[ "$(echo "$response" | grep "BUILD SUCCESS")" != "" ]]; then # success
       echo "Test successfully... ..."
       succ=$((succ + 1))
@@ -20,10 +25,34 @@ function execute_maven {
   fi
 }
 
-repo_test_report="$1"
+repo_test_report="$1" # taking from command parameter
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo "--------------------------------------------------------------------------------"
+log "Execute 'maven clean test' for current or the sub-folders"
+log "This can be improved by introducing a script but not directly calling maven so that it can include gradle project as well"
+log "How to use:"
+log "1. If there is command parameter, then it will enable 'repo_test_report' appending succ/failed results after execution;"
+log "2. It uses 'src' to indicate if current project is the maven projecet, or it is parent project with sub-folder of maven projects;"
+log "3. It is able to use parent maven project with maven modules,"
+log "   inside the parent maven project, there are sub-folders of maven projects."
+log "   Refer to 'microservice-dubbo - 1_dubbo-springboot2':"
+log "   microservice-dubbo"
+log "     |- run-repo-test.sh"
+log "     |- run-project-test.sh"
+log "     |- 1_dubbo-springboot2 (parent maven project)"
+log "        |- run-project-test.sh"
+log "        |- pom.xml (not with src, so will look for sub-folder to execute maven test)"
+log "        |- dubbo-service"
+log "          |- src"
+log "          |- pom.xml"
+log "        |- dubbo-provider"
+log "        |- dubbo-consumer"
+log "4. succ/failed count is with total of current 'run-project-test.sh' folder (total of  current maven project or sub-folders);"
+log "5. This script can be execute without trigger by 'run-repo-test.sh', for individual project execution only."
+log "Command: run-project-test.sh [repo_test_report]"
+echo "--------------------------------------------------------------------------------"
 log "v0.0.2 - 20200321"
+log "e.g. "
 log "$BASEDIR"
 log ""
 
@@ -41,7 +70,7 @@ else
   for d in *; do
     if [[ -d "$d" ]]; then
       cd "$d" || exit
-        execute_maven "$d"
+      execute_maven "$d"
       cd "$BASEDIR" || exit
     fi
   done
@@ -51,7 +80,7 @@ echo ""
 log "Total success: $succ; Total failed: $failed"
 echo ""
 
-# append the project test result into repo rest report
+# append the project test result into repo rest report with formatted by tab
 if [[ -f "$repo_test_report" ]]; then
   printf "%-50s   SUCCESS: %2d   FAILED: %2d\n" "${BASEDIR##*/}" $succ $failed >> "$repo_test_report"
 fi
--- run-repo-test.sh
+++ run-repo-test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# utilities to print with prefix
 function log() {
   local message=$1
   echo "[REPO TEST] $message"
 }
 
+# execute project test if there is pom.xml
 function execute_project_test() {
-  local r="$1"
+  local r="$1" # repo-test-report
   test="$(pwd)/run-project-test.sh"
+  # because this script is a wrapper script, the real execution logic would be 'run-project-test.sh'
   if [[ -f "${test}" ]]; then
     /bin/bash "${test}" "$1"
   fi
@@ -15,7 +18,6 @@ function execute_project_test() {
 
 clear
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-echo ""
 echo "-------------------------------------------------------------------------------------------"
 echo "  ____                ____                      _ _                     _____         _    "
 echo " |  _ \ _   _ _ __   |  _ \ ___ _ __   ___  ___(_) |_ ___  _ __ _   _  |_   _|__  ___| |_  "
@@ -25,17 +27,37 @@ echo " |_| \_\\__,_|_| |_| |_| \_\___| .__/ \___/|___/_|\__\___/|_|   \__, |   |
 echo "                               |_|                              |___/                      "
 echo "-------------------------------------------------------------------------------------------"
 # http://patorjk.com/software/taag/#p=display&f=Standard&t=Run%20Repository%20Test
+log "Execute 'run-project-test.sh' for current or the sub-folders"
+log "This is the wrapper of 'run-project-test.sh'"
+log "How to use:"
+log "1. By default it is single project because there is always 'run-project-test.sh' in root folder;"
+log "2. Put '.not-project-test' to root folder, then it will scaner sub-folders;"
+log "3. Print the test report with total succ/failed project test"
+log "4. Enable 2 levels by adding '.not-project-test' to root folder and copying 'run-project-test.sh' to sub-folders"
+log "   Refer to 'microservice-dubbo:"
+log "   microservice-dubbo"
+log "     |- .not-project-test (skip current folder - run-project-test.sh)"
+log "     |- run-repo-test.sh"
+log "     |- run-project-test.sh (will be skipped)"
+log "       |- 1_dubbo-springboot2"
+log "          |- run-project-test.sh (copied from root folder - will be triggered)"
+log "          |- pom.xml"
+log "          |- dubbo-service"
+log "          |- dubbo-provider"
+log "          |- dubbo-consumer"
+log "Command: run-repo-test.sh"
+echo "--------------------------------------------------------------------------------"
 log "v0.0.1 - 20200321"
 log "$BASEDIR"
 log ""
 
-# test report
+# test report - to handle the test result from 'run-project-test.sh'
 test_report="/tmp/repo-test-report-${BASEDIR##*/}-$(date "+%s")"
-# create test report json
 touch "$test_report"
 
-# execute all maven project(s) in this folder
+# this part can be enahnced in the future
 # if it is singel project
+# '.not-project-test' is a file additionally indicates a overriding of not to execute as single project 
 if [[ -f "run-project-test.sh" && ! -f ".not-project-test" ]]; then
   execute_project_test "$test_report"
 else
@@ -49,7 +71,7 @@ else
   done
 fi
 
-# test report
+# read lines from test report
 echo "-------------------------------------------------------------------------------------------"
 log "TEST REPORT"
 log ""
