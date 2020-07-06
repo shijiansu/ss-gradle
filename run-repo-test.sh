c5b3c29fc3f5ea39728d45163dfed97a0a3ace49
Shijian Su <sushijian.iss.nus@gmail.com>
2020-07-06 11:20:06 +0800

    Batch commit and push by gpush.sh at 2020-07-06T11:19:15

--- run-repo-test.sh
+++ run-repo-test.sh
@@ -2,17 +2,17 @@
 
 # utilities to print with prefix
 function log() {
-  local message=$1
-  echo "[REPO TEST] $message"
+  local message="${1}"
+  echo "[REPO TEST] ${message}"
 }
 
 # execute project test if there is pom.xml
 function execute_project_test() {
-  local r="$1" # repo-test-report
+  local r="${1}" # repo-test-report
   local test="$(pwd)/run-project-test.sh"
   # because this script is a wrapper script, the real execution logic would be 'run-project-test.sh'
   if [[ -f "${test}" ]]; then
-    /bin/bash "${test}" "$1"
+    /bin/bash "${test}" "${1}"
   fi
 }
 
@@ -26,25 +26,25 @@ echo " |_| \_\\__,_|_| |_| |_| \_\___| .__/ \___/|___/_|\__\___/|_|   \__, |   |
 echo "                               |_|                              |___/                      "
 echo "-------------------------------------------------------------------------------------------"
 # http://patorjk.com/software/taag/#p=display&f=Standard&t=Run%20Repository%20Test
-log "v0.0.1 - 20200321 - $BASEDIR"
+log "v0.0.1 - 20200321 - ${BASEDIR}"
 log ""
 
 # test report - to handle the test result from 'run-project-test.sh'
 test_report="/tmp/repo-test-report-${BASEDIR##*/}-$(date "+%s")"
-touch "$test_report"
+touch "${test_report}"
 
 # this part can be enahnced in the future
 # if it is singel project
 # 'run-project-test.skip' is a file additionally indicates a overriding of not to execute as single project 
 if [[ -f "run-project-test.sh" && ! -f "run-project-test.skip" ]]; then
-  execute_project_test "$test_report"
+  execute_project_test "${test_report}"
 else
   # for multiple projects
   for d in *; do
-    if [[ -d "$d" ]]; then
-      cd "$d" || exit
-      execute_project_test "$test_report"
-      cd "$BASEDIR" || exit
+    if [[ -d "${d}" ]]; then
+      cd "${d}" || exit
+      execute_project_test "${test_report}"
+      cd "${BASEDIR}" || exit
     fi
   done
 fi
@@ -54,6 +54,6 @@ echo "--------------------------------------------------------------------------
 log "TEST REPORT"
 log ""
 while IFS= read -r line; do
-  log "$line"
-done <"$test_report"
+  log "${line}"
+done <"${test_report}"
 echo "-------------------------------------------------------------------------------------------------------------------------"
