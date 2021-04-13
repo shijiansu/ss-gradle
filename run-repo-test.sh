ccdc5a785dc05decf904c78c41c108ce680cc13d
Shijian Su <sushijian.iss.nus@gmail.com>
2021-04-13 23:27:20 +0800

    Update project structure

--- run-repo-test.sh
+++ run-repo-test.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+# v0.0.3 - 20210411 - update run-project-test.sh, and make execution recurly
 
-# utilities to print with prefix
 function log() {
-  local message="${1}"
-  echo "[REPO TEST] ${message}"
+  echo "[REPO TEST] ${1}"
 }
 
-# execute project test if there is pom.xml
-function execute_project_test() {
-  local r="${1}" # repo-test-report
-  local test="$(pwd)/run-project-test.sh"
-  # because this script is a wrapper script, the real execution logic would be 'run-project-test.sh'
-  if [[ -f "${test}" ]]; then
-    /bin/bash "${test}" "${1}"
-  fi
+# wrap the project test script, would like individual project also able to run test
+function project_test_recurly() {
+  local repo_test_report="${1}" # taking from command parameter
+  
+  # if it is singel project
+  if [[ -f "run-project-test.sh" ]]; then
+    /bin/bash run-project-test.sh "${repo_test_report}"
+  else
+  # for multiple projects
+    for d in *; do
+      if [[ -d "${d}" ]]; then
+        cd "${d}" || exit
+        project_test_recurly "${repo_test_report}"
+        cd .. || exit
+      fi
+    done
+  fi  
 }
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
@@ -26,24 +34,22 @@ echo " |_| \_\\__,_|_| |_| |_| \_\___| .__/ \___/|___/_|\__\___/|_|   \__, |   |
 echo "                               |_|                              |___/                      "
 echo "-------------------------------------------------------------------------------------------"
 # http://patorjk.com/software/taag/#p=display&f=Standard&t=Run%20Repository%20Test
-log "v0.0.1 - 20200321 - ${BASEDIR}"
+log "v0.0.3 - 20210411 - ${BASEDIR}"
 log ""
 
 # test report - to handle the test result from 'run-project-test.sh'
 test_report="/tmp/repo-test-report-${BASEDIR##*/}-$(date "+%s")"
 touch "${test_report}"
 
-# this part can be enahnced in the future
-# if it is singel project
-# 'run-project-test.skip' is a file additionally indicates a overriding of not to execute as single project 
-if [[ -f "run-project-test.sh" && ! -f "run-project-test.skip" ]]; then
-  execute_project_test "${test_report}"
+# for single project
+if [[ -f "run-project-test.sh" ]]; then
+  project_test_recurly "${test_report}"
 else
   # for multiple projects
   for d in *; do
     if [[ -d "${d}" ]]; then
       cd "${d}" || exit
-      execute_project_test "${test_report}"
+      project_test_recurly "${test_report}"
       cd "${BASEDIR}" || exit
     fi
   done
