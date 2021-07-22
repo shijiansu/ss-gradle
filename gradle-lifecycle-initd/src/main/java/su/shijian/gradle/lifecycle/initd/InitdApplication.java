9fa6b26e0627d42c5517b5ada7e69008e679c9bd
Shijian Su <sushijian.iss.nus@gmail.com>
2021-07-23 00:36:03 +0800

    Add gradle init.d example

---
+++ gradle-lifecycle-initd/src/main/java/su/shijian/gradle/lifecycle/initd/InitdApplication.java
@@ -0,0 +1,12 @@
+package su.shijian.gradle.lifecycle.initd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitdApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InitdApplication.class, args);
+  }
+}
---
+++ gradle-lifecycle-initd/src/test/java/su/shijian/gradle/lifecycle/initd/InitdApplication.java
@@ -0,0 +1,11 @@
+package su.shijian.gradle.lifecycle.initd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitdApplication {
+
+  @Test
+  void contextLoads() {}
+}
