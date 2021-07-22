82b794909ddff6059394c889fc245bf1aa9dcbb0
Shijian Su <sushijian.iss.nus@gmail.com>
2021-07-22 09:07:04 +0800

    Add first try example

---
+++ gradle-plugin-first-try/src/main/java/su/shijian/gradle/plugin/first_try/FirstTryApplication.java
@@ -0,0 +1,13 @@
+package su.shijian.kubernetes.minikube.first_try;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesMinikubeFirstTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesMinikubeFirstTryApplication.class, args);
+	}
+
+}
---
+++ gradle-plugin-first-try/src/test/java/su/shijian/kubernetes/gradle/first_try/FirstTryApplicationTests.java
@@ -0,0 +1,13 @@
+package su.shijian.kubernetes.minikube.first_try;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesMinikubeFirstTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
