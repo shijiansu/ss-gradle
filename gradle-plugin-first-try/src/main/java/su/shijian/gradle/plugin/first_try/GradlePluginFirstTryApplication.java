9bb02b43d7dc5c7c1a1a46d2720256834cfc888f
Shijian Su <sushijian.iss.nus@gmail.com>
2021-07-23 00:36:19 +0800

    Add gradle plugin example

---
+++ gradle-plugin-first-try/src/main/java/su/shijian/gradle/plugin/first_try/GradlePluginFirstTryApplication.java
@@ -0,0 +1,13 @@
+package su.shijian.gradle.plugin.first_try;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlePluginFirstTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlePluginFirstTryApplication.class, args);
+	}
+
+}
---
+++ gradle-plugin-first-try/src/test/java/su/shijian/gradle/plugin/first_try/GradlePluginFirstTryApplicationTests.java
@@ -0,0 +1,13 @@
+package su.shijian.gradle.plugin.first_try;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlePluginFirstTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
